9dd37cef2fd8167f9f38e2a9552f30336e2d73f2
vczs <hi.vincz@gmail.com>
2022-12-21 10:52:06 +0800

    review

--- model/user.go
+++ model/user.go
@@ -7,7 +7,7 @@ type UserInfo struct {
 	Account    string `gorm:"unique;not null;comment:账号"`          // 唯一 不为空 注释
 	Name       string `gorm:"type:varchar(200)"`                   // 指定类型
 	Gender     string `gorm:"type:varchar(100);size:10;default:男"` // 指定类型 设置字段大小 默认值
-	Age        int    `gorm:"column:user_age"`                     // 零值类型 不为空
+	Age        int    `gorm:"column:user_age"`                     // 指定列名
 	Address    string `gorm:"index:addr;comment:地址"`               // 创建名为addr的索引 注释
 	Other      string `gorm:"-"`                                   // 忽略该字段(不在数据库表中关联)
 	gorm.Model        // gorm内置model 包含ID、创建时间、更新时间、删除时间
